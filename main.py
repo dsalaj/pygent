01eda1e4b9370262e49ab9da8e93a1bc1ee2e026
Darjan Salaj <salaj.au@gmail.com>
2016-07-25 22:33:04 +0200

    abstracted agent class; spreading zombies; moving humans

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ListProperty
 from kivy.clock import Clock
 from math import sin, cos, tan
 from random import randint
@@ -12,10 +12,10 @@ import numpy as np
 TODO:
   + rewrite existing code using numpy instead of python arrays
   + init fields using numpy broadcast vectorization
+  + add second type of agent
+  + play / impl. killing and spreading
+  + write abstract agent class
   - write helper detection functions for neighboring agents
-  - add second type of agent
-  - play / impl. killing and spreading
-  - write abstract agent class
 '''
 
 
@@ -25,23 +25,33 @@ class Field(BoxLayout):
     b = NumericProperty(1)
 
 
-class Zombie(Widget):
+class Agent(Widget):
     energy = 0
     row = 0
     col = 0
+    color = ListProperty([0.2, 0.2, 0.2, 1])
 
     def __init__(self, row, col, **kwargs):
-        super(Zombie, self).__init__(**kwargs)
+        super(Agent, self).__init__(**kwargs)
         self.row = row
         self.col = col
+
+
+class Zombie(Agent):
+    color = ListProperty([0, 1, 0, 1])
+
+
+class Human(Agent):
+    color = ListProperty([0.9, 0.8, 0.6, 1])
 
 
 class MtxCanvas(GridLayout):
     time = 0.0
     fields_np = None
     zombies = None
+    humans = None
 
-    def init_base(self, dim=(5, 5), zombie_num=1):
+    def init_base(self, dim=(5, 5), zombie_num=1, human_num=5):
         self.rows = dim[0]
         self.cols = dim[1]
         self.spacing = 1
@@ -54,6 +64,10 @@ class MtxCanvas(GridLayout):
         self.zombies = np.array([Zombie(randint(0, self.rows-1), randint(0, self.cols-1)) for _ in range(zombie_num)])
         for zombie in self.zombies.flat:
             self.fields_np[zombie.row, zombie.col].add_widget(zombie)
+
+        self.humans = np.array([Human(randint(0, self.rows - 1), randint(0, self.cols - 1)) for _ in range(human_num)])
+        for human in self.humans.flat:
+            self.fields_np[human.row, human.col].add_widget(human)
 
     def update(self, dt):
         # some logic for moving agents
@@ -75,6 +89,22 @@ class MtxCanvas(GridLayout):
         dead_zombies = np.array(dead_zombies)
         self.zombies = np.setdiff1d(self.zombies, dead_zombies)
 
+        ex_humans = []
+        new_zombies = []
+        for z in self.zombies:
+            for h in self.humans:
+                if z.row == h.row and z.col == h.col:
+                    if randint(0, 9) < 5:
+                        ex_humans.append(h)
+                        new_zombie = Zombie(z.row, z.col)
+                        new_zombies.append(new_zombie)
+                        self.fields_np[z.row, z.col].remove_widget(h)
+                        self.fields_np[z.row, z.col].add_widget(new_zombie)  # new zombie is born
+
+        ex_humans = np.array(ex_humans)
+        self.humans = np.setdiff1d(self.humans, ex_humans)
+        self.zombies = np.concatenate((self.zombies, new_zombies))
+
         # move zombies
         for zombie in self.zombies:        
             self.fields_np[zombie.row, zombie.col].remove_widget(zombie)
@@ -89,17 +119,30 @@ class MtxCanvas(GridLayout):
                 zombie.col = 0
             elif zombie.col < 0:
                 zombie.col += self.cols
-
             self.fields_np[zombie.row, zombie.col].add_widget(zombie)
 
-        # TODO: detect multiple zombies on one field
+        # move humans
+        for human in self.humans:
+            self.fields_np[human.row, human.col].remove_widget(human)
+            human.row += randint(-1, 1)
+            human.col += randint(-1, 1)
+            # wrap axes ~ torus world
+            if human.row >= self.rows:
+                human.row = 0
+            elif human.row < 0:
+                human.row += self.rows
+            if human.col >= self.cols:
+                human.col = 0
+            elif human.col < 0:
+                human.col += self.cols
+            self.fields_np[human.row, human.col].add_widget(human)
 
 
 class MtxApp(App):
     def build(self):
         mtx = MtxCanvas()
-        mtx.init_base(dim=(15, 15), zombie_num=25)
-        Clock.schedule_interval(mtx.update, 1.0 / 30.0)
+        mtx.init_base(dim=(30, 30), zombie_num=10, human_num=100)
+        Clock.schedule_interval(mtx.update, 1.0 / 6.0)
         return mtx
 
 if __name__ == '__main__':
