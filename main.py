779316ca4d246094858e85aab49de823ce937420
Darjan Salaj <salaj.au@gmail.com>
2016-07-27 08:54:13 +0200

    generalized agent movement

--- main.py
+++ main.py
@@ -29,12 +29,25 @@ class Agent(Widget):
     energy = 0
     row = 0
     col = 0
-    color = ListProperty([0.2, 0.2, 0.2, 1])
+    color = ListProperty([0, 0, 0, 1])
 
     def __init__(self, row, col, **kwargs):
         super(Agent, self).__init__(**kwargs)
         self.row = row
         self.col = col
+
+    def random_move(self, world_rows, world_cols):
+        self.row += randint(-1, 1)
+        self.col += randint(-1, 1)
+        # wrap axes - simulate torus world
+        if self.row >= world_rows:
+            self.row = 0
+        elif self.row < 0:
+            self.row += world_rows
+        if self.col >= world_cols:
+            self.col = 0
+        elif self.col < 0:
+            self.col += world_cols
 
 
 class Zombie(Agent):
@@ -46,44 +59,40 @@ class Human(Agent):
 
 
 class MtxCanvas(GridLayout):
-    time = 0.0
-    fields_np = None
+    fields = None
     zombies = None
     humans = None
+
+    def agents(self):
+        return np.concatenate([self.zombies, self.humans])
 
     def init_base(self, dim=(5, 5), zombie_num=1, human_num=5):
         self.rows = dim[0]
         self.cols = dim[1]
         self.spacing = 1
 
-        self.fields_np = np.array([[Field() for _ in range(self.cols)] for _ in range(self.rows)])
-        for field in self.fields_np.flat:
+        self.fields = np.array([[Field() for _ in range(self.cols)] for _ in range(self.rows)])
+        for field in self.fields.flat:
             self.add_widget(field)
-        # self.fields_np = np.reshape(self.fields_np, newshape=(self.rows, self.cols))
 
         self.zombies = np.array([Zombie(randint(0, self.rows-1), randint(0, self.cols-1)) for _ in range(zombie_num)])
         for zombie in self.zombies.flat:
-            self.fields_np[zombie.row, zombie.col].add_widget(zombie)
+            self.fields[zombie.row, zombie.col].add_widget(zombie)
 
         self.humans = np.array([Human(randint(0, self.rows - 1), randint(0, self.cols - 1)) for _ in range(human_num)])
         for human in self.humans.flat:
-            self.fields_np[human.row, human.col].add_widget(human)
+            self.fields[human.row, human.col].add_widget(human)
 
     def update(self, dt):
         # some logic for moving agents
-
-        self.fields_np[1, 1].r = sin(self.time)
-        self.fields_np[3, 2].g = cos(self.time)
-        self.fields_np[2, 4].b = tan(self.time)
-        self.time += dt
 
         dead_zombies = []
         for z1 in self.zombies:
             for z2 in self.zombies:
                 if z1 is not z2:
                     if z1.row == z2.row and z1.col == z2.col:
-                        self.fields_np[z1.row, z1.col].remove_widget(z1)
-                        self.fields_np[z1.row, z1.col].remove_widget(z2)
+                        self.fields[z1.row, z1.col].remove_widget(z1)
+                        self.fields[z1.row, z1.col].remove_widget(z2)
                         dead_zombies.append(z1)
                         dead_zombies.append(z2)
         dead_zombies = np.array(dead_zombies)
@@ -98,51 +107,25 @@ class MtxCanvas(GridLayout):
                         ex_humans.append(h)
                         new_zombie = Zombie(z.row, z.col)
                         new_zombies.append(new_zombie)
-                        self.fields_np[z.row, z.col].remove_widget(h)
-                        self.fields_np[z.row, z.col].add_widget(new_zombie)  # new zombie is born
+                        self.fields[z.row, z.col].remove_widget(h)
+                        self.fields[z.row, z.col].add_widget(new_zombie)  # new zombie is born
 
         ex_humans = np.array(ex_humans)
         self.humans = np.setdiff1d(self.humans, ex_humans)
         self.zombies = np.concatenate((self.zombies, new_zombies))
 
-        # move zombies
-        for zombie in self.zombies:        
-            self.fields_np[zombie.row, zombie.col].remove_widget(zombie)
-            zombie.row += randint(-1, 1)
-            zombie.col += randint(-1, 1)
-            # wrap axes - torus world
-            if zombie.row >= self.rows:
-                zombie.row = 0
-            elif zombie.row < 0:
-                zombie.row += self.rows
-            if zombie.col >= self.cols:
-                zombie.col = 0
-            elif zombie.col < 0:
-                zombie.col += self.cols
-            self.fields_np[zombie.row, zombie.col].add_widget(zombie)
-
-        # move humans
-        for human in self.humans:
-            self.fields_np[human.row, human.col].remove_widget(human)
-            human.row += randint(-1, 1)
-            human.col += randint(-1, 1)
-            # wrap axes ~ torus world
-            if human.row >= self.rows:
-                human.row = 0
-            elif human.row < 0:
-                human.row += self.rows
-            if human.col >= self.cols:
-                human.col = 0
-            elif human.col < 0:
-                human.col += self.cols
-            self.fields_np[human.row, human.col].add_widget(human)
+        # move agents
+        for a in self.agents():
+            self.fields[a.row, a.col].remove_widget(a)
+            a.random_move(self.rows, self.cols)
+            self.fields[a.row, a.col].add_widget(a)
 
 
 class MtxApp(App):
     def build(self):
         mtx = MtxCanvas()
         mtx.init_base(dim=(30, 30), zombie_num=10, human_num=100)
-        Clock.schedule_interval(mtx.update, 1.0 / 6.0)
+        Clock.schedule_interval(mtx.update, 1.0 / 40.0)
         return mtx
 
 if __name__ == '__main__':
